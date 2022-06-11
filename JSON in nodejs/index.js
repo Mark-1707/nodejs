d335af7c974a83b22242706bf67fabd8d03bd752
Mark-1707 <shiroteomkar6@gmail.com>
2022-06-11 14:51:51 +0530

    JSON

---
+++ JSON in nodejs/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const data = {
+    name : "Omkar",
+    age : 26,
+    channel : "Mark"
+}
+
+// console.log(data)
+// console.log(data.name)
+
+// const jsonData = JSON.stringify(data)
+// console.log("JSON data : " + jsonData)
+
+// const objData = JSON.parse(jsonData)
+// console.log("Object data : " + objData.channel)
+
+const jsonData = JSON.stringify(data);
+
+fs.writeFile("readFile.json", jsonData, (err) => {
+    console.log("Done")
+});
+
+fs.readFile("readFile.json", "utf8", (err, data) => {
+    console.log(data);
+    const orgData = JSON.parse(data);
+    console.log(orgData)
+})
