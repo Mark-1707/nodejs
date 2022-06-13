f604fdc7649af92062925587d640536d6b195222
Mark-1707 <shiroteomkar6@gmail.com>
2022-06-13 20:08:27 +0530

    Create Collection and Schema

--- MongoDB/Connect Mongo and Node/src/app.js
+++ MongoDB/Connect Mongo and Node/src/app.js
@@ -2,6 +2,28 @@ const mongoose = require('mongoose')
 // const express = require('express')
 
 //connection to mongoDB database
-mongoose.connect('mongodb://localhost:27017/demo')
+mongoose.connect('mongodb://localhost:27017/mark')
 .then(()=>console.log('connection successful'))
 .catch(err => console.log(err));
+
+
+//Define a shema for the database
+const playlistSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+//Create Collection
+const Playlist = mongoose.model("Playlist", playlistSchema);
+
